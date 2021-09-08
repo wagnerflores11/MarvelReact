fea26fef6816f674759e874622fafc6492fced5a
Wagner Flores <wagner.flores@altenburg.com.br>
2021-09-07 21:44:33 -0300

    add pagination

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Footer from '../components/footer';
-import api from '../services/api';
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import "./App.css";
+import ReactPaginate from "react-paginate";
+import Footer from "../components/footer";
+import api from "../services/api";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -33,15 +33,18 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(8),
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   cardContent: {
     flexGrow: 1,
+  },
+  fab: {
+    float: "right",
   },
   footer: {
     backgroundColor: theme.palette.background.default,
@@ -49,27 +52,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export default function Album() {
+export default function Marvel() {
   const classes = useStyles();
-  
-  const [id, setid] = useState([])
-  const [title, settitle] = useState([])
+  const [heros, setHeros] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
 
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const pageCount = Math.ceil(heros.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   useEffect(() => {
-
-    async function loadid() {
-      const response = await api.get()
-      setid(response.data)
-
-    }
-
-    loadid();
-  })
-
+    const loadHeros = async () => {
+      const response = await api.get();
+        setHeros(response.data.data.results);
+        console.log(response)
+    };
+    
+    loadHeros();
+  }, []);
   
+  console.log(heros);
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -77,89 +85,73 @@ export default function Album() {
         <Toolbar>
           <CameraIcon className={classes.icon} />
           <Typography variant="h6" color="inherit" noWrap>
-            Album layout
+            LOGO
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-     
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Album layout
             </Typography>
           </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
-          
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+            {heros
+              .slice(pagesVisited, pagesVisited + usersPerPage)
+              .map((hero) => (
+                <Grid item key={hero.id} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <img
+                      src={`${hero.thumbnail.path}/portrait_incredible.${hero.thumbnail.extension}`}
+                      alt=""
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        <h2>{hero.name}</h2>
+                      </Typography>
+                      <Typography>
+                        <p>{hero.description}</p>
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button
+                        className={classes.fab}
+                        size="small"
+                        color="primary"
+                      >
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                  {/*<Posts posts={currentPosts} loading={loading} />*/}
+                </Grid>
+              ))}
           </Grid>
-          
-          <Grid container spacing={4}>
-            {title.map(result => (
-              <Grid item key={result.id} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    src={`${result.thumbnail.path}.${result.thumbnail.extension}`}
-                    alt={result.title}
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                     {result.title}
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-          
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          />
         </Container>
       </main>
-
-      <footer className={classes.footer}>        
+      <footer className={classes.footer}>
         <Footer />
       </footer>
-
     </React.Fragment>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import md5 from 'md5';
 
-const publicKey = '0dc01c44bb43144fbd26b9b6bc9f5e66';
-const privateKey = '4f0dbd449a4316755c808e1c50b98c9ba23204d0';
+const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+const privateKey = process.env.REACT_APP_PRIVATE_KEY;
 const ts = Date.now();  
+
 
 
 const api = axios.create({
     baseURL: `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${md5(`${ts}${privateKey}${publicKey}`)}`, 
     version: 'Cable',
 });
+
+
 
 export default api; 
